0107b39741b8830fb4a454fbe3c1e3a022d6d6d2
Jesper <jespersandstrom@gmail.com>
2023-12-16 13:43:07 +0100

    Day 12

--- bin/main.ml
+++ bin/main.ml
@@ -956,6 +956,104 @@ module Day_11 = struct
   let solve_2 input = solve input ~expansion_factor:1000000
 end
 
+module Day_12 = struct
+  let matches_template_4
+    (template : char array)
+    (group_index, group)
+    previous_index
+    ~check_end
+    =
+    let is_legal_spring char = Char.( = ) char '?' || Char.( = ) char '#' in
+    let is_legal_space char = Char.( = ) char '?' || Char.( = ) char '.' in
+    if group_index >= Array.length template then failwith "invalid group index";
+    let matches = ref true in
+    for index = previous_index to group_index + group do
+      matches
+      := !matches
+         && (index >= Array.length template
+             ||
+             let template_char = template.(index) in
+             if index < group_index || index >= group_index + group
+             then is_legal_space template_char
+             else is_legal_spring template_char)
+    done;
+    if check_end
+    then
+      for index = group_index + group to Array.length template - 1 do
+        if not (is_legal_space template.(index)) then matches := false
+      done;
+    !matches
+  ;;
+
+  let solve template groups =
+    let groups_length = Array.length groups in
+    let template_length = String.length template in
+    let template_array = template |> String.to_array in
+    let cache =
+      List.range 0 groups_length
+      |> List.map ~f:(fun _ ->
+        List.range 0 template_length |> List.map ~f:(fun _ -> -1) |> Array.of_list)
+      |> Array.of_list
+    in
+    let rec possible_positions starting_index group_index =
+      if group_index = groups_length
+      then 1
+      else if cache.(group_index).(starting_index) <> -1
+      then cache.(group_index).(starting_index)
+      else (
+        let group = groups.(group_index) in
+        let groups_after =
+          Array.foldi
+            groups
+            ~f:(fun i acc x -> if i > group_index then acc + x + 1 else 0)
+            ~init:0
+        in
+        let first_possible_index = starting_index in
+        let last_possible_index = template_length - group - groups_after in
+        let result =
+          List.range first_possible_index (last_possible_index + 1)
+          |> List.map ~f:(fun index ->
+            if matches_template_4
+                 template_array
+                 (index, group)
+                 starting_index
+                 ~check_end:(group_index = groups_length - 1)
+            then possible_positions (index + group + 1) (group_index + 1)
+            else 0)
+          |> Util.sum
+        in
+        cache.(group_index).(starting_index) <- result;
+        result)
+    in
+    possible_positions 0 0
+  ;;
+
+  let solve_1 input =
+    List.map input ~f:(fun line ->
+      let split = String.split_on_chars ~on:[ ' ' ] line in
+      let template = List.nth_exn split 0 in
+      let groups = List.nth_exn split 1 |> String.split_on_chars ~on:[ ',' ] in
+      let groups = List.map ~f:int_of_string groups |> Array.of_list in
+      solve template groups)
+    |> Util.sum
+  ;;
+
+  let solve_2 input =
+    let copy string ~times ~sep =
+      List.range 0 times |> List.map ~f:(fun _ -> string) |> String.concat ~sep
+    in
+    List.map input ~f:(fun line ->
+      let split = String.split_on_chars ~on:[ ' ' ] line in
+      let _template = List.nth_exn split 0 in
+      let template = copy _template ~times:5 ~sep:"?" in
+      let _groups = List.nth_exn split 1 in
+      let groups = copy _groups ~times:5 ~sep:"," |> String.split_on_chars ~on:[ ',' ] in
+      let groups = List.map ~f:int_of_string groups |> Array.of_list in
+      solve template groups)
+    |> Util.sum
+  ;;
+end
+
 let () =
   print_endline "";
   let day_1 = Util.read_file "inputs/day1.txt" |> List.map ~f:String.to_list in
@@ -1053,5 +1151,13 @@ let () =
   print_endline ("Part 1 " ^ Util.to_result ~result:day_11_part_1 ~expected:9742154);
   let day_11_part_2 = Util.measure_ns Day_11.solve_2 day_11 in
   print_endline ("Part 2 " ^ Util.to_result ~result:day_11_part_2 ~expected:411142919886);
+  print_endline "";
+  (* / *)
+  print_endline "Day 12";
+  let day_12 = Util.read_file "inputs/day12.txt" in
+  let day_12_part_1 = Util.measure_ns Day_12.solve_1 day_12 in
+  print_endline ("Part 1 " ^ Util.to_result ~result:day_12_part_1 ~expected:6488);
+  let day_12_part_2 = Util.measure_ns Day_12.solve_2 day_12 in
+  print_endline ("Part 2 " ^ Util.to_result ~result:day_12_part_2 ~expected:815364548481);
   print_endline ""
 ;;
